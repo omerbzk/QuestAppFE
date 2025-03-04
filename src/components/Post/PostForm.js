08e819035fcb9afaf50e1add31dcc197d3064a09
omerbzk <bozkurtomer150@gmail.com>
2025-03-04 17:38:10 +0300

    PostForm alert snackbar added , title text clean added, create post added,

--- src/components/Post/PostForm.js
+++ src/components/Post/PostForm.js
@@ -3,19 +3,19 @@ import { styled } from "@mui/material/styles";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
 import CardContent from "@mui/material/CardContent";
-import CardActions from "@mui/material/CardActions";
-import Collapse from "@mui/material/Collapse";
 import Avatar from "@mui/material/Avatar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import { red } from "@mui/material/colors";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import ShareIcon from "@mui/icons-material/Share";
 import { makeStyles } from "@mui/styles";
-import InsertCommentIcon from '@mui/icons-material/InsertComment';
 import { Link } from "react-router-dom";
-import { Button, InputAdornment, OutlinedInput } from "@mui/material";
-import SendIcon from '@mui/icons-material/Send';
+import {
+  Alert,
+  Button,
+  InputAdornment,
+  OutlinedInput,
+  Snackbar,
+} from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
 
 const useStyles = makeStyles({
   root: {
@@ -26,20 +26,20 @@ const useStyles = makeStyles({
   },
   media: {
     height: 0,
-    paddingTop: '56.25%', // 16:9
+    paddingTop: "56.25%", // 16:9
   },
   expand: {
-    transform: 'rotate(0deg)',
-    marginLeft: 'auto',
-    transition: 'transform 0.3s',
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: "transform 0.3s",
   },
   avatar: {
-    background: 'linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)',
+    background: "linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)",
   },
   link: {
-    textDecoration: 'none',
-    boxShadow: 'none',
-    color: 'white',
+    textDecoration: "none",
+    boxShadow: "none",
+    color: "white",
   },
   container: {
     display: "block", // Flex yerine block kullanarak alt alta diziyoruz
@@ -58,82 +58,115 @@ const ExpandMore = styled((props) => {
 }));
 
 function PostForm(props) {
-  const { title, text, userId, userName } = props;
+  const { userId, userName, refreshPosts } = props;
   const classes = useStyles();
-  const [expanded, setExpanded] = useState(false);
-  const [liked, setLiked] = useState(false);
+  const [text, setText] = useState("");
+  const [title, setTitle] = useState("");
+  const [isSent, setIsSent] = useState(false);
 
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
+  const savePost = () => {
+    fetch("/v1/posts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        userId: userId,
+        title: title,
+        text: text,
+      }),
+    })
+      .then((res) => res.json())
+      .catch((error) => console.error("Error:", error));
   };
 
-  const handleLike = () => {
-    setLiked(!liked);
-  }
+  const handleSubmit = () => {
+    savePost();
+    setIsSent(true);
+    setTitle("");
+    setText("");
+    refreshPosts();
+  };
+
+  const handleTitle = (value) => {
+    setTitle(value);
+    setIsSent(false);
+  };
+
+  const handleText = (value) => {
+    setText(value);
+    setIsSent(false);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setIsSent(false);
+  };
 
   return (
-  <div className={classes.container}>
-    <Card className={classes.root}>
-      <CardHeader
-        avatar={
-          <Link className={classes.link} to={{ pathname: "/users/" + userId }}>
-          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            {userName.charAt(0).toUpperCase()}
-          </Avatar>
-          </Link>
-        }
-        title={<OutlinedInput 
-            id="outlined-adorment-amount"
-            multiline
-            placeholder="Title"
-            inputProps={{ maxLength: 25 }}
-            fullWidth>
-            </OutlinedInput>}
-      />
-      <CardContent>
-        <Typography variant="body2" sx={{ color: "text.secondary" }}>
-        <OutlinedInput 
-            id="outlined-adorment-amount"
-            multiline
-            placeholder="Text"
-            inputProps={{ maxLength: 500 }}
-            fullWidth
-            endAdornment = {
-                <InputAdornment position="end">
-                    <Button
-                    variant="contained"
-                    style={{ background: 'linear-gradient(45deg, #2196f3 30%, #21cbf3 90%)' }}
-                    endIcon={<SendIcon />}>
-                        Post
-                    </Button>
-                 </InputAdornment>
-            }
+    <div>
+      <Snackbar open={isSent} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="success">
+          Post sent successfully!
+        </Alert>
+      </Snackbar>
+      <Card className={classes.root}>
+        <CardHeader
+          avatar={
+            <Link
+              className={classes.link}
+              to={{ pathname: "/users/" + userId }}
             >
-            </OutlinedInput>
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton
-        onClick={handleLike}
-        aria-label="add to favorites">
-          <FavoriteIcon style={liked? { color: "red"} : null} />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-        <ExpandMore
-          expand={expanded}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <InsertCommentIcon />
-        </ExpandMore>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent></CardContent>
-      </Collapse>
-    </Card>
+              <Avatar className={classes.avatar} aria-label="recipe">
+                {userName.charAt(0).toUpperCase()}
+              </Avatar>
+            </Link>
+          }
+          title={
+            <OutlinedInput
+              id="outlined-adorment-amount"
+              multiline
+              placeholder="Title"
+              inputProps={{ maxLength: 25 }}
+              fullWidth
+              value={title}
+              onChange={(i) => handleTitle(i.target.value)}
+            ></OutlinedInput>
+          }
+        />
+        <CardContent>
+          <Typography variant="body2" sx={{ color: "text.secondary" }}>
+            <OutlinedInput
+              id="outlined-adorment-amount"
+              multiline
+              placeholder="Text"
+              inputProps={{ maxLength: 500 }}
+              fullWidth
+              value={text}
+              onChange={(i) => handleText(i.target.value)}
+              endAdornment={
+                <InputAdornment position="end">
+                  <Button
+                    variant="contained"
+                    style={{
+                      background:
+                        "linear-gradient(45deg, #2196f3 30%, #21cbf3 90%)",
+                      color: "white",
+                    }}
+                    onClick={handleSubmit}
+                    endIcon={<SendIcon />}
+                  >
+                    Post
+                  </Button>
+                </InputAdornment>
+              }
+            ></OutlinedInput>
+          </Typography>
+        </CardContent>
+      </Card>
     </div>
   );
 }
